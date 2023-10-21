6ad92324074f29e7ee399b7636a390b6e1c32051
golfziller <pongtorngolf@gmail.com>
2023-10-21 13:35:11 +0700

    fix ui

--- components/show_basketball.tsx
+++ components/show_basketball.tsx
@@ -12,9 +12,9 @@ type ShowImgProps = {
 const ShowBasketballImage:NextPage<ShowImgProps> = ({id, }) => {
   return (
     <OverLab id="img" className='w-full '>
-    <div className="2xl:w-full 2xl:max-w-[1350px]  pt-[80px]   mx-auto 
+    <div className="2xl:w-full 2xl:max-w-[1350px] xl:container pt-[80px]   mx-auto 
      flex md:flex-row-reverse md:pt-[50px] lg:pt-0 flex-row md:justify-start justify-center">
-       <img id={id} alt="my picture eiei"  className='md:w-[440px] lg:w-[60%]  xl:w-[64.5%]'  />
+       <img id={id} alt="my picture eiei"  className='md:w-[440px] lg:w-[60%]  xl:w-[64%]'  />
     </div>
   </OverLab>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -51,7 +51,7 @@ const Main: NextPage<{name: string}> =({name}) => {
         detail={dataPage.basketball[0].desc}
         classNameDivSubTitle='lg:mb-6 md:mb-4 hidden md:block md:pb-1 lg:pb-0'
         classNameDivContent="lg:basis-[50%] lg:pb-6 md:pb-0 pt-3"
-        classNameParent='md:pl-6 lg:pl-0 md:pt-[55px] lg:pt-0'
+        classNameParent='md:pl-8 lg:pl-0 md:pt-[55px] lg:pt-0'
         descriptionClass='lg:pb-5 md:pb-3'></Step>
 
       <Step 
@@ -59,7 +59,7 @@ const Main: NextPage<{name: string}> =({name}) => {
         subTitle={dataPage.basketball[1].subTitle}
         detail={dataPage.basketball[1].desc}
         classNameDivSubTitle='lg:mb-6 md:mb-4 hidden md:block'
-        classNameParent='md:pl-6 lg:pl-0' 
+        classNameParent='md:pl-8 lg:pl-0' 
         classNameDivContent='!lg:pt-10 md:pt-[65px] md:pb-[30px] ' 
         isReverse  
         descriptionClass=' pb-8 ' 
